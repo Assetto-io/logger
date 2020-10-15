e99364a27c6b021163006ed96fe862423d701937
Denis Nefedov <nefedov.d.d@gmail.com>
2020-10-15 08:23:14 +0300

    init: first version of simple logger

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # logger
 logger engine based on zap framework
+
+## Installation
+
+```bash
+go get github.com/assetto-io/logger
+```
+
+## Usage
+In order to use the library you need to import the corresponding package:
+
+```go
+import "github.com/assetto-io/logger"
+```
+
+## Performing Logging calls
+The ``logger`` module provides convenient methods that you can use to perform different logging calls.
+
+### Info
+
+```go
+logger.Info("some kind of event")
+```
+
+### Error
+
+```go
+if err := whoops.MethodWithError; err != nil {
+    logger.Error("some kind of event", err)
+}
+```
+
+### Working with tags
+
+```go
+url := "http://test.com"
+request := NewCustomRequest()
+
+if err := whoops.PerformRequest(url, request); err != nil {
+    logger.Error("some kind of event", err, logger.Field(url, request))
+}
+```
---
+++ logger.go
@@ -0,0 +1,55 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	log *zap.Logger
+)
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{"stdout"},
+		Encoding:    "json",
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey:   "msg",
+			LevelKey:     "level",
+			TimeKey:      "time",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+	var err error
+	log, err = logConfig.Build()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetLog() *zap.Logger {
+	return log
+}
+
+func Field(key string, value interface{}) zap.Field {
+	return zap.Any(key, value)
+}
+
+func Debug(msg string, tags ...zap.Field) {
+	log.Debug(msg, tags...)
+	log.Sync()
+}
+
+func Info(msg string, tags ...zap.Field) {
+	log.Info(msg, tags...)
+	log.Sync()
+}
+
+func Error(msg string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.Error(msg, tags...)
+	log.Sync()
+}
